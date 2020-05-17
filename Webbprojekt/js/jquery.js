b144720b2eff054b78efd779c2f5746472dd395a
Arvid <arvid.sandstedt@gmail.com>
2020-05-17 03:25:48 +0200

    grafisk manual

---
+++ Webbprojekt/js/jquery.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  console.log("started");
+      
+    $(".logga").animate({
+        width: "120px",
+        height: "120px", 
+         }, 2200 );
+      });
+
+   
+      
+            
